912d3e1ad01b77aa4dfa9dbf50ee8ccf9978ba52
katafuchix <katafuchix@gmail.com>
2025-07-30 20:43:39 +0900

    Initial Commit

---
+++ SwiftUI_PermissionExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_PermissionExample
+//
+//  Created by cano on 2025/07/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_PermissionExample/SwiftUI_PermissionExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PermissionExampleApp.swift
+//  SwiftUI_PermissionExample
+//
+//  Created by cano on 2025/07/30.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PermissionExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
